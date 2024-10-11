475f782a8988b1d98e4b2f005cbb70ac4077c871
Ferdiyanto <akun.ferdiyanto@gmail.com>
2024-10-12 05:53:02 +0700

    semerbk

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import iconTwSvg from './assets/svg/icontw.svg';
 import iconLnSvg from './assets/svg/iconln.svg';
 import iconGitSvg from './assets/svg/icongit.svg';
 import hereSvg from './assets/svg/here.svg';
+import smrbSvg from './assets/svg/smrb.svg';
 import segitSvg from './assets/svg/segit.svg';
 
 function App() {
@@ -135,22 +136,38 @@ function App() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-3">
+        {/* <div className="flex flex-col gap-3">
         <h1 className='text-white mt-8 font-semibold text-xl'>My Projects</h1>
         <div className="flex flex-row gap-3 text-white">
           <a href="">UI UX</a>
           <a href="">Web Application</a>
           <a href="">Desain Grafis</a>
         </div>
-        </div>
+        </div> */}
 
         <div className="bg-white w-full rounded-two mt-6 flex flex-col xl:flex-row ">
           <div className="p-4 xl:w-1/2 h-60 relative flex overflow-hidden">
+            <div className="flex flex-col gap-3">
             <h1 className="font-medium text-primary text-3xl">Here.stockroom</h1>
+            <p className='text-[#989898]'>Blending Technical Knowledge with Creative Solutions</p>
+            </div>
             <img className="absolute bottom-0 text-center -right-20 justify-between " src={segitSvg} alt="Decoration" />
           </div>
           <div className="xl:w-1/2">
             <img src={hereSvg} alt="Here Illustration" />
+          </div>
+        </div>
+
+        <div className="bg-[#EA580C] w-full rounded-two mt-6 flex flex-col xl:flex-row ">
+          <div className="p-4 xl:w-1/2 h-60 relative flex overflow-hidden">
+          <div className="flex flex-col gap-3">
+            <h1 className="font-medium text-primary text-3xl">Semerbak Coffe</h1>
+            <p className='text-[#989898]'>Blending Technical Knowledge with Creative Solutions</p>
+            </div>
+            <img className="absolute bottom-0 text-center -right-20 justify-between " src={segitSvg} alt="Decoration" />
+          </div>
+          <div className="xl:w-1/2">
+            <img className='w-full' src={smrbSvg} alt="Here Illustration" />
           </div>
         </div>
       </div>
