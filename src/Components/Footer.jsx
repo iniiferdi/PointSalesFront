08450a672ae7b7d62428bf9bbcb110aa9716ec44
Ferdiyanto <akun.ferdiyanto@gmail.com>
2024-10-12 06:17:26 +0700

    footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+
+    return(
+        <>
+        <div className="px-4 xl:px-16 top-0 py-6 flex items-center justify-center bg-primary text-white pt-32">
+        <p>Made by me iniiferdi ❤ @2024️</p>
+        </div>
+        </>
+    )
+
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './Pages/App.jsx'
+import Navbar from './Components/Navbar.jsx'
+import Footer from './Components/Footer.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar/>
     <App />
+    <Footer />
   </StrictMode>,
 )
