e9f6eea59eb9c824c5ccdfd07c2a8824c3ae1a16
Ferdiyanto <akun.ferdiyanto@gmail.com>
2024-10-24 19:50:57 +0700

    upp

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <main className=' flex mx-auto justify-center items-center p-6 bg-primary'>
-        <div className="w-full  max-w-7xl">
+        <div className="w-full  max-w-7xl flex flex-col gap-12 items-center justify-center">
         <Navbar/>
         <Hero/>
         </div>
